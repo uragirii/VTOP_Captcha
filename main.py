1817ede3ba62635a9f4e2378438598a5b6b8eb04
Apoorv Kansal <apoorvkansalak@gmail.com>
2020-03-29 22:48:34 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# This script will attempt to solve captcha present on the the VTOP VIT Login Page.
+# Script designed to be worked with VITask page. Some code may be copied from there.
+# Ref : https://github.com/Codebotics/VITask
+# author : Cherub
+
+
+#imports
